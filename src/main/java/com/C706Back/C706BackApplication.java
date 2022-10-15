aee5ca402d3e806d556bfab9e4dc8cef21ed895b
MartinKun <martinkunbrc@hotmail.com>
2022-10-15 12:34:55 -0300

    Adding allowingOrigins and allowedMethods

--- src/main/java/com/C706Back/C706BackApplication.java
+++ src/main/java/com/C706Back/C706BackApplication.java
@@ -18,7 +18,7 @@ public class C706BackApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST","PUT", "DELETE");
+						.allowedOrigins("http://localhost:3000", "*");
 			}
 
 
